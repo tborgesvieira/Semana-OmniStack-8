e92be04e97d186f20f5a33e45a762db9e5b952bb
Thiago Borges Vieira <thiago@upgradesolutions.com.br>
2019-08-10 17:40:17 -0300

    aula 4

--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import { 
     KeyboardAvoidingView, 
     Platform, 
@@ -8,15 +9,31 @@ import {
     TouchableOpacity, 
     Text } from 'react-native';
 
+import api from '../services/api';
+
 import logo from '../assets/logo.png';
 
 export default function Login({ navigation }){
     const [user, setUser] = useState('');
 
-    function handleLogin(){
-        console.log(user);
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user =>{
+            if(user){
+                navigation.navigate('Main', { user })
+            }
+        })
+    }, []);
 
-        //navigation.navigate('Main');
+    async function handleLogin(){        
+        const response = await api.post('/devs',{
+            username: user
+        });
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+
+        navigation.navigate('Main', {_id});
     }
     
     return (
--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,12 +1,102 @@
 import React from 'react';
 import { 
     View,
-    Text} from 'react-native';
+    Text,
+    Image,
+    SafeAreaView,
+    StyleSheet,
+    TouchableOpacity} from 'react-native';
+
+import logo from '../assets/logo.png';
+import like from '../assets/like.png';
+import dislike from '../assets/dislike.png';
 
 export default function Main(){
     return (
-        <View>
-            <Text>Main</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+
+            <View style={styles.cardsContainer}>
+                <View style={styles.card}>
+                    <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4'}} />
+                    <View style={styles.footer}>
+                        <Text style={styles.name}>Diego Fernandes</Text>
+                        <Text style={styles.bio} numberOfLines={3}>CTO na @Rocketseat. Apaixonado por Javascript, ReactJS, React Native, NodeJS e todo ecossistema em torno dessas tecnologias.</Text>
+                    </View>
+                </View>
+
+                <View style={styles.card}>
+                    <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4'}} />
+                    <View style={styles.footer}>
+                        <Text style={styles.name}>Diego Fernandes</Text>
+                        <Text style={styles.bio} numberOfLines={3}>CTO na @Rocketseat. Apaixonado por Javascript, ReactJS, React Native, NodeJS e todo ecossistema em torno dessas tecnologias.</Text>
+                    </View>
+                </View>
+
+                <View style={styles.card}>
+                    <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4'}} />
+                    <View style={styles.footer}>
+                        <Text style={styles.name}>Diego Fernandes</Text>
+                        <Text style={styles.bio} numberOfLines={3}>CTO na @Rocketseat. Apaixonado por Javascript, ReactJS, React Native, NodeJS e todo ecossistema em torno dessas tecnologias.</Text>
+                    </View>
+                </View>
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Image source={like} />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    logo:{
+        marginTop: 30
+    },
+    container:{
+        flex:1,
+        backgroundColor: '#f5f5f5',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    cardsContainer:{
+        flex: 1,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        maxHeight: 500
+    },
+    card: {
+        borderWidth:1,
+        borderColor: '#ddd',
+        borderRadius: 8,
+        margin: 30,
+        overflow: 'hidden',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+    },
+    avatar:{
+        flex: 1,
+        height:300,
+    },
+    footer:{
+        backgroundColor: '#fff',
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+    },
+    name:{
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#333'
+    },
+    bio:{
+        fontSize: 14,
+        color: '#999',
+        marginTop: 5,
+        lineHeight: 18
+    }
+});
