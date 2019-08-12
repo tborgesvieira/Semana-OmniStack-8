fd6baaca55bbd1e5d31e37b1a8cad1e26d2890d0
Thiago Borges Vieira <thiago@upgradesolutions.com.br>
2019-08-11 22:57:12 -0300

    Aula 4 - Quase acabando

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import { 
     View,
     Text,
@@ -7,44 +8,88 @@ import {
     StyleSheet,
     TouchableOpacity} from 'react-native';
 
+import api from '../services/api';
+
 import logo from '../assets/logo.png';
+import acabou from '../assets/acabou.png';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
 
-export default function Main(){
+export default function Main({ navigation }){
+    const id = navigation.getParam('user');
+    const [users, setUsers] = useState([]);
+    
+    useEffect(()=>{
+        async function loadUsers(){
+            const response = await api.get('/devs',{
+                headers:{
+                    user: id,
+                }
+            });
+
+            setUsers(response.data);
+        }
+
+        loadUsers();
+    }, [id]);
+    
+    async function handleLike(id){
+        await api.post(`/devs/${id}/likes`,null,{
+            headers: {
+                user:id
+            }
+        });
+
+        setUsers(users.filter(user => user._id !== id));
+    }
+
+    async function handleDislike(id){
+        await api.post(`/devs/${id}/dislikes`,null,{
+            headers: {
+                user:match.params.id
+            }
+        });
+
+        setUsers(users.filter(user => user._id !== id));
+    }
+
+    async function handleLogout(){
+        await AsyncStorage.clear();
+
+        navigation.navigate('Login');
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <Image style={styles.logo} source={logo} />
-
+            <TouchableOpacity onPress={handleLogout}>
+                <Image style={styles.logo} source={logo} />
+            </TouchableOpacity>
             <View style={styles.cardsContainer}>
-                <View style={styles.card}>
-                    <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4'}} />
+                { users.length === 0                
+                 ? 
+                 <View>
+                    <Text style={styles.empty}>Acabou</Text>
+                    <Image style={styles.acabou} source={acabou} />
+                  </View>
+                 :
+                 users.map((user, index) =>(
+                    <View key={user._id} style={[styles.card, { zIndex: users.length - index}]}>
+                    <Image style={styles.avatar} source={{uri: user.avatar}} />
                     <View style={styles.footer}>
-                        <Text style={styles.name}>Diego Fernandes</Text>
-                        <Text style={styles.bio} numberOfLines={3}>CTO na @Rocketseat. Apaixonado por Javascript, ReactJS, React Native, NodeJS e todo ecossistema em torno dessas tecnologias.</Text>
+                        <Text style={styles.name}>{user.name}</Text>
+                        <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
                     </View>
                 </View>
-
-                <View style={styles.card}>
-                    <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4'}} />
-                    <View style={styles.footer}>
-                        <Text style={styles.name}>Diego Fernandes</Text>
-                        <Text style={styles.bio} numberOfLines={3}>CTO na @Rocketseat. Apaixonado por Javascript, ReactJS, React Native, NodeJS e todo ecossistema em torno dessas tecnologias.</Text>
-                    </View>
-                </View>
-
-                <View style={styles.card}>
-                    <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4'}} />
-                    <View style={styles.footer}>
-                        <Text style={styles.name}>Diego Fernandes</Text>
-                        <Text style={styles.bio} numberOfLines={3}>CTO na @Rocketseat. Apaixonado por Javascript, ReactJS, React Native, NodeJS e todo ecossistema em torno dessas tecnologias.</Text>
-                    </View>
-                </View>
+                ))
+                }
             </View>
 
             <View style={styles.buttonContainer}>
                 <TouchableOpacity style={styles.button}>
-                    <Image source={like} />
+                    <Image source={dislike} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Image source={like} />                    
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
@@ -60,6 +105,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#f5f5f5',
         alignItems: 'center',
         justifyContent: 'space-between'
+    },
+    empty:{
+        alignSelf: 'center',
+        color: '#999',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    acabou:{
+        alignSelf: 'center',        
     },
     cardsContainer:{
         flex: 1,
@@ -98,5 +152,27 @@ const styles = StyleSheet.create({
         color: '#999',
         marginTop: 5,
         lineHeight: 18
+    },
+
+    buttonContainer:{
+        flexDirection: 'row',
+        marginBottom: 30,
+    },
+    button:{
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOpacity: 0.05,
+        shadowRadius: 2,
+        shadowOffset:{
+            width:0,
+            height:2
+        }
     }
 });
